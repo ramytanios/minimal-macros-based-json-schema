4b1983de70f3f25a871ac8426a170f78f8925cd1
Ramy Tanios <ramy.tanios@gmail.com>
2024-03-17 22:45:25 +0100

    wi

--- annotations/shared/main/scala/annotations/Repr.scala
+++ annotations/shared/main/scala/annotations/Repr.scala
@@ -1,13 +1,17 @@
 package schema
 
 trait Repr[A] {
+
   def repr: (String, String)
+
 }
 
 object Repr {
+
   def apply[A](implicit ev: Repr[A]): Repr[A] = ev
 
   def factory[A](f: () => (String, String)): Repr[A] = new Repr[A] {
     def repr: (String, String) = f()
   }
+
 }
